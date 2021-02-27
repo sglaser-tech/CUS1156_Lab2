8e146e72b09c5fe8dda6c23c21658e2b5b27fee0
Sarah <sarah.glaser20@my.stjohns.edu>
2021-02-27 18:42:27 -0500

    CUS1156_Lab2

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,43 @@
+/* Project: Lab2
+ * Class: UniqueWords.java
+ * Author: Sarah Glaser
+ * Date: February 28th, 2021
+ * This program counts the number of distinct elements.
+ */
+
 import java.util.ArrayList;
 
-public class UniqueWords
-{
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
-   }
+public class UniqueWords {
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+	// ----------------------------------------------------------------------------------------------------------
+	// This method counts the number of unique strings in a list.
+	public static int countUnique(ArrayList<String> list) {
+		int count = 0;
+		for (int i = 0; i < list.size(); i++) {
+			boolean isUnique = true;
+			for (int j = 0; j < i; j++) {
+				if (list.get(i) == list.get(j)) {
+					isUnique = false;
+				}
+			}
+			if (isUnique) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<String> words = new ArrayList<>();
+		words.add("apple");
+		words.add("orange");
+		words.add("blackboard");
+		words.add("apple");
+		words.add("orange");
+		words.add("sun");
+		words.add("moon");
+
+		int unique = countUnique(words);
+		System.out.println(words + " has " + unique + " unique words");
+	}
 }
